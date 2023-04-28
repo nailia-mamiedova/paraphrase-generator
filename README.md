31c21e102c05a1e6bedb339f2799877fee2c9df6
Nailia Mamiedova <nailia.mamiedova.work@gmail.com>
2023-04-28 03:07:59 +0300

    first try

---
+++ README.md
@@ -0,0 +1,9 @@
+# Paraphrase
+
+For start program
+
+`python main.py`
+
+After server started paste this link to browser
+
+localhost:5000/paraphrase?tree=(S%20(NP%20(NP%20(DT%20The)%20(JJ%20charming)%20(NNP%20Gothic)%20(NNP%20Quarter)%20)%20(,%20,)%20(CC%20or)%20(NP%20(NNP%20Barri)%20(NNP%20G%C3%B2tic)%20)%20)%20(,%20,)%20(VP%20(VBZ%20has)%20(NP%20(NP%20(JJ%20narrow)%20(JJ%20medieval)%20(NNS%20streets)%20)%20(VP%20(VBN%20filled)%20(PP%20(IN%20with)%20(NP%20(NP%20(JJ%20trendy)%20(NNS%20bars)%20)%20(,%20,)%20(NP%20(NNS%20clubs)%20)%20(CC%20and)%20(NP%20(JJ%20Catalan)%20(NNS%20restaurants)%20)%20)%20)%20)%20)%20)%20)
---
+++ main.py
@@ -0,0 +1,55 @@
+import itertools
+from flask import Flask, request, jsonify
+from nltk.tree import Tree
+
+app = Flask(__name__)
+
+
+def generate_phrases(tree, phrases):
+    if tree.label() == 'NP':
+        phrase = ' '.join(tree.leaves())
+        phrases.append(phrase)
+    for child in tree:
+        if isinstance(child, Tree):
+            generate_phrases(child, phrases)
+
+
+def replace_noun_phrases(tree, phrase_permutation):
+    for subtree in tree.subtrees():
+        if subtree.label() == 'NP':
+            for i, phrase in enumerate(phrase_permutation):
+                if ' '.join(subtree.leaves()) == phrase:
+                    subtree.clear()
+                    subtree.insert(0, Tree.fromstring('(' + phrase + ')'))
+
+
+@app.route('/paraphrase', methods=['GET'])
+def paraphrase():
+    tree_string = request.args.get('tree')
+    limit = int(request.args.get('limit', 20))
+    tree = Tree.fromstring(tree_string)
+    phrases = []
+    generate_phrases(tree, phrases)
+    noun_phrases = [phrase for phrase in phrases if ' ' in phrase]
+    paraphrases = []
+    for i in range(2, len(noun_phrases)+1):
+        for combination in itertools.combinations(noun_phrases, i):
+            for permutation in itertools.permutations(combination):
+                new_tree = tree.copy(deep=True)
+                replace_noun_phrases(new_tree, permutation)
+                paraphrase = str(new_tree).replace('\n', '')  # Remove newline character
+                if paraphrase not in paraphrases:
+                    paraphrases.append(paraphrase)
+                    if len(paraphrases) == limit:
+                        break
+            else:
+                continue
+            break
+        else:
+            continue
+        break
+    return jsonify({'paraphrases': [{'tree': tree} for tree in paraphrases]})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
